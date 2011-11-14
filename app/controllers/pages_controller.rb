3014e91640cb6eee2095b05a2bcdffa8cf821ce8
Pierre Hellwig <pierre.hellwig@gmail.com>
2011-11-14 22:51:12 +0100

    Improvements

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 require 'digest'
 class PagesController < ApplicationController
+  
   def home
     @title = "Home"
+  end
+  
+  def impressum
+    @title = "Impressum"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Medi::Application.routes.draw do
 get "pages/home"
 
 match '/home',       :to => 'pages#home'
+match '/impressum',  :to => 'pages#impressum'
 
 root :to => 'pages#home'
 end
