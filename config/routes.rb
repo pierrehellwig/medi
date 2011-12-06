dab04737fcb5b29645a95b5538fb9898db41ef78
Pierre Hellwig <pierre.hellwig@gmail.com>
2011-12-07 00:58:46 +0100

    Thanks Page hinzugefuegt

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Medi::Application.routes.draw do
 get "pages/home"
+get "pages/impressum"
+get "pages/thanks"
 
 match '/home',       :to => 'pages#home'
 match '/impressum',  :to => 'pages#impressum'
+match '/thanks',     :to => 'pages#thanks'
 
 root :to => 'pages#home'
 end
